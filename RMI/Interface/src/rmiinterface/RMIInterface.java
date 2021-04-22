d19a760efec0a57760f171b360d0fe42b9e5d219
Miguel Angel <leonaguirrem3@gmail.com>
2021-04-22 01:34:07 -0500

    Se agregó la clase Server

--- RMI/Interface/src/rmiinterface/RMIInterface.java
+++ RMI/Interface/src/rmiinterface/RMIInterface.java
@@ -1,2 +1,8 @@
-package rmiinterface;public interface RMIInterface {
+package rmiinterface;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface RMIInterface extends Remote{
+    public String helloTo(String name) throws RemoteException;
 }
